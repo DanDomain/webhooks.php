2daaf64d9ccd63533f6f9df56ad052a6f87d1e8c
mlo <mlo@dandomain.dk>
2016-08-12 07:56:32 +0200

    added the first bare steps towards a proper example.

---
+++ complex.php
@@ -0,0 +1,36 @@
+<?php
+
+//get the incoming POST body as a string
+$incomingJsonString = file_get_contents('php://input');
+
+//decode the JSON string to an associative array.
+$eventArray = json_decode($incomingJsonString, true);
+
+foreach($eventArray as $event) {
+
+    if($event['ObjectType'] == "Product") {
+        //the event is for a product.
+
+        if($event['NewValues'] === null && $event['OldValues'] !== null) {
+            //product was deleted.
+            $productNumber = $event['OldValues']['ObjectIdentifier'];
+
+            
+        } else if($event['NewValues'] !== null && $event['OldValues'] === null) {
+            //product was created.
+            $productNumber = $event['NewValues']['ObjectIdentifier'];
+
+
+        } else if($event['NewValues'] !== null && $event['OldValues'] !== null) {
+            //product was modified.
+
+            $oldProductNumber = $event['OldValues']['ObjectIdentifier'];
+            $newProductNumber = $event['NewValues']['ObjectIdentifier'];
+
+            
+        }
+    }
+
+}
+
+?>
---
+++ simple.php
@@ -0,0 +1,11 @@
+<?php
+
+//get the incoming POST body as a string
+$incomingJsonString = file_get_contents('php://input');
+
+//write the body into a file which you can then view
+$file = fopen("last_incoming_json.txt", "w") or throw new Exception('Could not write to file.');;
+fwrite($file, $txt);
+fclose($file);
+
+?>
